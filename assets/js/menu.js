c8c590807d3a7f20e5ea5721bd8fd2f65df3f420
sam-maji15 <86354352+sam-maji15@users.noreply.github.com>
2021-08-02 11:06:50 +0530

    Add files via upload

---
+++ assets/js/menu.js
@@ -0,0 +1,15 @@
+const btn_filter = document.getElementById('btn_filter')
+
+btn_filter.onfocus = displayAndAlignDropDown
+
+function displayAndAlignDropDown() {
+  const dropDown = document.querySelector('.con_drop_down')
+  dropDown.style.display = "block"
+  
+  let btn_right = (window.innerWidth - btn_filter.getBoundingClientRect().right).toString()
+  let btn_bottom = btn_filter.getBoundingClientRect().bottom.toString()
+  
+  dropDown.style.right = btn_right + 'px'
+  dropDown.style.top = btn_bottom + 'px'
+  //dropDown.style.transform = "translateX(50%)"
+}
